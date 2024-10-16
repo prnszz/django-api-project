dc5aa14c1a84acaefbaee9f896794e6d750200b0
Pengshuo Qiu <149804000+prnszz@users.noreply.github.com>
2024-10-16 11:02:09 +0900

    Update changes

--- your_project/settings.py
+++ your_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,7 +127,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
