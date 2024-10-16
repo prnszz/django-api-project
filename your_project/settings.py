f0501ac79cfc5534e99b26cb58fc647e79595a9b
Pengshuo Qiu <149804000+prnszz@users.noreply.github.com>
2024-10-16 10:57:43 +0900

    Update changes

--- your_project/settings.py
+++ your_project/settings.py
@@ -82,7 +82,8 @@ DATABASES = {
     }
 }
 
-
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
